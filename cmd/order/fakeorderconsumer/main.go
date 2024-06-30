1ad7844830ecb0eb2e3e9024ecca2d6fc391d0f1
Benyamin <benyaminmahmoudyan@gmail.com>
2024-06-30 12:33:38 +0330

    feat: fake consumer

---
+++ cmd/order/fakeorderconsumer/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/iam-benyamin/hellofresh/param/orderparam"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://hellofresh:food@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"orders", // name
+		"direct", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	// Declare a queue
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,          // queue name
+		"created_order", // routing key
+		"orders",        // exchange
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			var message orderparam.Message
+			err := json.Unmarshal(d.Body, &message)
+			if err != nil {
+				log.Printf("Error decoding JSON: %s", err)
+				continue
+			}
+
+			log.Printf("Received a message: %+v", message)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
